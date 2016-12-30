802800f11102ea276672f4ccba4d3588d14e487c
McKenna Rowe <mrowe@fender.com>
2016-12-30 15:21:07 -0800

    Add files via upload

--- app.jsx
+++ app.jsx
@@ -12,7 +12,7 @@ var PLAYERS = [
   {
  name: "Larry Bird",
  score: 77,
- id:3,
+ id: 3,
  }
 ]
 
@@ -28,20 +28,24 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 };
 
-function Counter(props) {
-  return (
+var Counter = React.createClass({
+    propTypes: {
+    score: React.PropTypes.number.isRequired,
+    },
+  render: function() {
+   return (
      <div className="counter">
         <button className="counter-action decrement">-</button>
-        <div className="counter-score"> {props.score} </div>
+        <div className="counter-score"> {this.props.score} </div>
         <button className="counter-action increment">+</button>
       </div>
   );
-}
+  }
+});
 
-Counter.propTypes = {
-  score: React.PropTypes.number.isRequired,
-}
-  
+
+
+
 
 function Player(props) {
   return (
